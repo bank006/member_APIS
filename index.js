5ecddf72d862a98c1b2314e0a3b9bb0be0c8fc65
bank006 <88577275+bank006@users.noreply.github.com>
2024-06-26 19:24:24 +0700

    add api

--- index.js
+++ index.js
@@ -68,6 +68,55 @@ app.get('/users/api/v1/get_users', async (req, res) => {
         res.status(400).json({ error: 'Error find users' });
     }
 })
+
+app.post('/users/api/v1/create_user', async (req, res) => {
+    try {
+        const uuid_user = uuidv4()
+        const { data } = req.body
+        const username = data.username
+        const phonenum = data.phonenum
+        const checkphone = await conn.query('SELECT * FROM users WHERE phonenum = $1', [phonenum])
+        if (checkphone.rows.length > 0) {
+            res.json(checkphone)
+        } else {
+            const result = await conn.query('INSERT INTO users (fullname , phonenum , uuid) VALUES($1 , $2 , $3)', [username, phonenum, uuid_user])
+            res.json(
+                result
+            )
+        }
+
+    } catch (error) {
+        console.error('create user feild:', error.message);
+        res.status(400).json({ error: 'create user feild:' });
+    }
+})
+
+app.post('/v1/check_user', async (req, res) => {
+    try {
+        const { phonenum } = req.body
+        const result = await conn.query('SELECT * FROM users WHERE phonenum = $1 ', [phonenum])
+        if (result.rows[0]) {
+            res.json(result.rows)
+        } else {
+            res.status(400).json({ error: 'User not found' })
+        }
+
+    } catch (error) {
+        console.error('find user feild:', error.message);
+        res.status(400).json({ error: 'find user feild:' });
+    }
+})
+
+app.post('/v1/get_userpoint' ,async(req ,res)=>{
+    try{
+        const {uuid }= req.body
+        const result = await conn.query('SELECT * FROM users INNER JOIN point ON users.uuid = point.uuid_user WHERE point.uuid_user = $1',[uuid])
+        res.json(result.rows)
+    }catch(error){
+        console.error('find userpoint feild:', error.message);
+        res.status(400).json({ error: 'find userpoint feild:' });
+    }
+})
 //endusers
 
 const port = process.env.PORT || 3003;
